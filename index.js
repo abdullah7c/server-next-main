a86123b452a7c955cea0a88faed47d967526c564
Syed_Zaidi <syedabdullah.7ctech@gmail.com>
2021-12-16 05:21:52 -0800

    updated

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const db = require("./models")
-
-const {Users} = require("./models")
 
 app.use(express.json({ extended:false }))
 app.use(cors())
@@ -14,17 +11,7 @@ app.get("/", (req,res)=>{
     res.send("Welcome to Page")
 })
 
-app.get("/users", async(req,res)=>{
-    try {
-        const result = await Users.findOne()
-        res.json(result)
-    } catch (error) {
-        res.json("Error Recieved")
-    }
-});
 
-db.sequelize.sync().then((req) => {
     app.listen(port, () => {
         console.log(`Server is booming on port 5000`)
     })
-})
